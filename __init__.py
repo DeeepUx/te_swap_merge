5db0840ef37a4748fdaac688a14da67716735f23
DeeepUx <131966001+DeeepUx@users.noreply.github.com>
2024-07-25 09:42:51 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,10 @@
+# __init__.py
 from modules import script_callbacks
 
 def on_ui_tabs():
     from .te_swap_merge import on_ui_tabs as te_on_ui_tabs
+    print("Registering TE Swap & Merge tab...")
     return [te_on_ui_tabs()]
 
+print("Initializing TE Swap & Merge extension...")
 script_callbacks.on_ui_tabs(on_ui_tabs)
