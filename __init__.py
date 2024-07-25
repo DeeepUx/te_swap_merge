4854dce007db2c8d846ad4649d0cdf9edac8d7f8
DeeepUx <131966001+DeeepUx@users.noreply.github.com>
2024-07-25 09:40:31 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,7 @@
-# __init__.py
-import os
 from modules import script_callbacks
 
 def on_ui_tabs():
-    from te_swap_merge import on_ui_tabs as te_on_ui_tabs
+    from .te_swap_merge import on_ui_tabs as te_on_ui_tabs
     return [te_on_ui_tabs()]
 
 script_callbacks.on_ui_tabs(on_ui_tabs)
