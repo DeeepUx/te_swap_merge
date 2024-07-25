6a2b987a63bf101d38663ae5ff115893628074ce
DeeepUx <131966001+DeeepUx@users.noreply.github.com>
2024-07-25 09:38:33 -0400

    Update install.py

--- install.py
+++ install.py
@@ -6,3 +6,4 @@ if not launch.is_installed("safetensors"):
 
 if not launch.is_installed("transformers"):
     launch.run_pip("install transformers", "requirements for TE Swap & Merge extension")
+    
